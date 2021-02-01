2c3dae74e842e9c3d6b5bc911017bffd4fcb1eea
Kishan Kr Sharma <skishan781@gmail.com>
2021-02-01 12:18:55 +0530

    first commit

---
+++ app/src/main/java/com/heycode/aizi/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.heycode.aizi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
