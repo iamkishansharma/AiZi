c995da56e7563c692a2fb5fc30ec21f4ee1bc33f
Kishan Kr Sharma <skishan781@gmail.com>
2021-03-10 16:46:34 +0530

    different screens added

--- app/src/main/java/com/heycode/aizi/MainActivity.kt
+++ app/src/main/java/com/heycode/aizi/MainActivity.kt
@@ -2,10 +2,49 @@ package com.heycode.aizi
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.heycode.aizi.dailyroutine.DailyRoutineFragment
+import com.heycode.aizi.dashboard.DashbaordFragment
+import com.heycode.aizi.profile.ProfileFragment
 
 class MainActivity : AppCompatActivity() {
+    lateinit var fragment: Fragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
+    }
+
+    private fun init() {
+        loadFragment(DashbaordFragment())
+        findViewById<BottomNavigationView>(R.id.bottom_navigation).setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_dashboard -> {
+                    fragment = DashbaordFragment()
+                    supportActionBar?.title = resources.getString(R.string.dashboard)
+                }
+                R.id.navigation_daily_routine -> {
+                    fragment = DailyRoutineFragment()
+                    supportActionBar?.title = resources.getString(R.string.daily_routine)
+                }
+                R.id.navigation_profile -> {
+                    fragment = ProfileFragment()
+                    supportActionBar?.title = resources.getString(R.string.profile)
+                }
+            }
+
+            loadFragment(fragment)
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment): Boolean {
+        if (fragment != null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit()
+            return true
+        }
+        return false
     }
 }
