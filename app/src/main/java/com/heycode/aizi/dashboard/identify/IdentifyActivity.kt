c0ef38b0ecce11e88afd039021605cc4ae6fbdea
Kishan Kr Sharma <skishan781@gmail.com>
2021-03-25 16:15:09 +0530

    code managed

--- app/src/main/java/com/heycode/aizi/dashboard/identify/IdentifyActivity.kt
+++ app/src/main/java/com/heycode/aizi/dashboard/identify/IdentifyActivity.kt
@@ -8,23 +8,17 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
-import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
-import com.google.firebase.storage.FirebaseStorage
 import com.heycode.aizi.R
 
 class IdentifyActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
-    private lateinit var fab: ExtendedFloatingActionButton
 
     //firebase
     private lateinit var mFirestore: FirebaseFirestore
 
-    //firebase
-    private lateinit var mAuth: FirebaseAuth
-    private lateinit var mStorage: FirebaseStorage
     private var peopleAdapter: PeopleRVAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,11 +34,6 @@ class IdentifyActivity : AppCompatActivity() {
         mFirestore = FirebaseFirestore.getInstance()
 
         recyclerView = findViewById(R.id.identify_recycler_view)
-        fab = findViewById(R.id.identify_fab)
-
-        fab.setOnClickListener {
-            startActivity(Intent(this, AddImageActivity::class.java))
-        }
 
         val path = "peoples" + user!!.uid
         val query: Query = mFirestore.collection(path)
@@ -62,7 +51,7 @@ class IdentifyActivity : AppCompatActivity() {
     }
 
     private fun checkForAccess(path: String) {
-        if (mFirestore.collection(path).id.isEmpty()) {
+        if (mFirestore.collection(path).id.isNullOrEmpty()) {
             val builder = AlertDialog.Builder(this)
             builder.setTitle("Not allowed to use this feature!")
                 .setCancelable(false)
